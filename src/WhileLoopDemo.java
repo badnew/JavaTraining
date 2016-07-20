2bdee628bff8ca22c87f5ec1de72779626e66e11
badnew <badnew@mail.ru>
2016-07-20 10:35:17 +0300

    while loop with continue operator

---
+++ src/WhileLoopDemo.java
@@ -0,0 +1,23 @@
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        String[] friends = new String[20];
+        friends[0] = "Maria";
+        friends[1] = "Olga";
+        friends[2] = "Matilda";
+        friends[18] = new String("Oksana");  // another way to initiate the string
+        friends[19] = "Vera";
+
+        int totalElements = friends.length;
+        int i = 0;
+        while (i < totalElements) {
+            if (friends[i] == null) {
+                i++;
+                continue;
+            }
+            System.out.println("I love " + friends[i]);
+            i++;
+        }
+
+        System.out.println("The iteration is over");
+    }
+}
