3dbeb1a639d8392ab2491e7bd5aef155534e25e6
badnew <badnew@mail.ru>
2016-07-20 19:06:54 +0300

    add lines to avoid an exception if there aren't three arguments in TestTax configure

--- src/fain/unit3/walkthrough/TestTax.java
+++ src/fain/unit3/walkthrough/TestTax.java
@@ -4,6 +4,13 @@ import fain.unit3.walkthrough.homebox.Tax;
 
 public class TestTax {
     public static void main(String[] args) {
+
+        if (args.length != 3) {
+            System.out.println("Sample usage of the program: " +
+            " java TestTax 50000 2 NJ");
+            System.exit(0);
+        }
+
         Tax t = new Tax();
 
         double gi = Double.parseDouble(args[0]);
