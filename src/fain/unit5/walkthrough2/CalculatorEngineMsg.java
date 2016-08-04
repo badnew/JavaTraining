8eb283039058d6d93db342fa349b80bb4e3a7e3d
badnew <badnew@mail.ru>
2016-08-04 20:39:20 +0300

    unit5

---
+++ src/fain/unit5/walkthrough2/CalculatorEngineMsg.java
@@ -0,0 +1,14 @@
+package fain.unit5.walkthrough2;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+public class CalculatorEngineMsg implements ActionListener {
+
+    public void actionPerformed(ActionEvent e){
+        JOptionPane.showConfirmDialog(null,
+                "Something happened...",
+                "Just a test",
+                JOptionPane.PLAIN_MESSAGE);
+    }
+}
